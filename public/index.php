86997e7b0cca1e5a07e34038d77bfc9e9232f609
brnkgabriel <brnkgabriel@users.noreply.github.com>
2016-08-12 18:05:48 -0500

    Completed profile page

--- public/index.php
+++ public/index.php
@@ -69,33 +69,74 @@
 		<div id="body" class="container">
 			<div class="row">
 				<div class="col-md-4">
-					<h3>Biodata</h3>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h3 class="panel-title">Biodata</h3>
+						</div>
+						<div class="panel-body">
+							<div class="row">
+								<div class="col-md-6" align="center">
+									<p><img src=<?php echo "images/".$user['first'].$user['last'].'.png'; ?> alt="Image Icon"/></p>
+									<p><?php echo $user['first'].' '.$user['last']; ?></p> 
+									<p><?php echo $user['age'].'yrs'; ?></p>
+								</div>
+								<div class="col-md-6">
+									<p>Pos: <?php echo $user['position']; ?></p>
+									<p>Agg: <?php echo $user['totalAggregate']; ?></p> 
+								</div>
+							</div>  
+						</div>
+					</div>
 				</div>
-				<div class="col-md-8">
-					<h3>Attributes</h3>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				<div class="col-md-8"> 
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h3 class="panel-title">Attributes</h3>
+						</div>
+						<div class="panel-body">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+							quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+							consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+							cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+							proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+						</div>
+					</div>
 				</div>
 			</div>
 			<div class="row">
-				<div class="col-md-12">
-					<h3>Statistics</h3>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				<div class="col-md-12"> 
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h3 class="panel-title">Statistics</h3>
+						</div>
+						<div class="panel-body table-responsive">
+							<table class="table table-bordered table-condensed"> 
+								<thead>
+									<tr>
+										<th>Day</th>
+										<th>Exc</th>
+										<th>Typ</th>
+										<th>Src</th>
+										<th>Sco</th>
+										<th>Age</th>
+										<th>Agg</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<td>Jan 3 2016</td>
+										<td>Eternal Life</td>
+										<td>Review</td>
+										<td>Andrew Wommack</td>
+										<td>0</td>
+										<td>17</td>
+										<td>0.000</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+					</div>
 				</div> 
 			</div>
 		</div>
