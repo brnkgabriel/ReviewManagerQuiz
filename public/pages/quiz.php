3663432f1a0a56da53a1fedda5fd6d6dd2ca13df
brnkgabriel <brnkgabriel@users.noreply.github.com>
2016-08-20 00:22:42 -0500

    Included The Relationship Accordion Panel

--- public/pages/quiz.php
+++ public/pages/quiz.php
@@ -21,7 +21,7 @@
 						<div class="panel-heading">
 							<h4 class="panel-title"> 
 								<a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
-									Actions: Paula White
+									<strong>[Attitude]</strong> Actions by Paula White
 								</a>
 							</h4>
 						</div>
@@ -35,13 +35,27 @@
 						<div class="panel-heading">
 							<h4 class="panel-title">
 								<a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
-									Repositioning for Exploits: Bishop David Oyedepo
+									<strong>[Career]</strong> Repositioning for Exploits by Bishop David Oyedepo
 								</a>
 							</h4>
 						</div>
 						<div id="collapseTwo" class="panel-collapse collapse">
 							<div class="panel-body">
-								<p><iframe width="560" height="315" src="https://www.youtube.com/embed/V6i395WNbAA" frameborder="0" allowfullscreen></iframe></p>
+								<p><iframe width="560" height="315" src="https://www.youtube.com/embed/V6i395WNbAA?list=PLFzgN2iKn8bT6SP4MGQ7FHDkT4s_Fj3Qp" frameborder="0" allowfullscreen></iframe></p>
+							</div>
+						</div>
+					</div>
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h4 class="panel-title">
+								<a data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
+									<strong>[Relationship]</strong> Single Again by Myles Munroe
+								</a>
+							</h4>
+						</div>
+						<div id="collapseThree" class="panel-collapse collapse">
+							<div class="panel-body">
+								<p><iframe width="560" height="315" src="https://www.youtube.com/embed/6Frru5jFo6A?list=PLFzgN2iKn8bT6SP4MGQ7FHDkT4s_Fj3Qp" frameborder="0" allowfullscreen></iframe></p>
 							</div>
 						</div>
 					</div>
